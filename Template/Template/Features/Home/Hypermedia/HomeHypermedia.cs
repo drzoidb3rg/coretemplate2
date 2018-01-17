4cddc4e0e67356fddb1dedeee099d8bf6fd040e7
martin bailey <martin.bailey@gmail.com>
2018-01-17 11:12:38 +0000

    init

---
+++ Template/Template/Features/Home/Hypermedia/HomeHypermedia.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Template.Framework.Hypermedia;
+
+namespace Template.Features.Home.Hypermedia
+{
+	public class HomeHypermedia : HydraClass
+	{
+
+		public HomeHypermedia()
+		{
+			
+		}
+
+		public HomeHypermedia(IContext ctx)
+		{
+			Id = ctx.Link<HomeHypermedia>().Id;
+		}
+    }
+}
---
+++ Template/Template/Framework/IContext.cs
@@ -0,0 +1,31 @@
+﻿using Template.Framework.Hypermedia;
+
+namespace Template
+{
+	public interface IContext
+	{
+		HydraLink Link<T>() where T : HydraClass;
+
+		HydraLink Link<T>(int id) where T : HydraClass;
+
+		HydraLink Link<T>(object values) where T : HydraClass;
+
+		HydraLink Link(string routeName, object values);
+
+		HydraLink Link(HydraClass media, object values);
+
+		string CurrentUrl();
+
+		string Host();
+
+		string Scheme();
+	}
+
+	public static class ContextExtensions
+	{
+		public static string GetId<T>(this IContext ctx, int id) where T : HydraClass
+		{
+			return ctx.Link<T>(id).Id;
+		}
+	}
+}
